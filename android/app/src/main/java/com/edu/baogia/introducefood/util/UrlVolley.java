b65c12515d89fccb0611392261bb62741f68f3e6
QuangBao <zzgiabaozzbui@gmail.com>
2022-06-14 17:07:56 +0700

    start

--- android/app/src/main/java/com/edu/baogia/introducefood/util/UrlVolley.java
+++ android/app/src/main/java/com/edu/baogia/introducefood/util/UrlVolley.java
@@ -1,8 +1,10 @@
 package com.edu.baogia.introducefood.util;
 
+import static com.edu.baogia.introducefood.util.idwifi.ipWifi;
+
 public class UrlVolley {
 
-    public String IP_ADDRESS="http://192.168.1.55";
+    public String IP_ADDRESS="http://" + ipWifi;
     public String url=IP_ADDRESS+"/ReviewFoods/service/thang/";
 
 
--- android/app/src/main/java/com/edu/baogia/introducefood/util/idwifi.java
+++ android/app/src/main/java/com/edu/baogia/introducefood/util/idwifi.java
@@ -4,7 +4,7 @@ public class idwifi {
 //    public static String ipWifi = "192.168.43.61";
     //public static String ipWifi = "192.168.1.55";
 
-    public static String ipWifi = "192.168.0.113";
+    public static String ipWifi = "192.168.1.55";
     public String urlThang="http://"+ipWifi+"/ReviewFoods/service/thang/";
     public String urlNinh="http://"+ipWifi+"/ReviewFoods/service/ninh/";
 }
